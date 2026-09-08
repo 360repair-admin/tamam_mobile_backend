b14af840bf9908cb204fcb9158bd1a3b8a96c270
Inzamam Tahir <inzamamtahir@gmail.com>
2026-09-08 10:00:07 +0300

    .

--- app/services/jwt/decoder.rb
+++ app/services/jwt/decoder.rb
@@ -1,6 +1,6 @@
 module Jwt
   class Decoder
-    SECRET = Rails.application.credentials.secret_key_base
+    SECRET = Rails.application.secret_key_base
 
     def self.call(token)
       payload, = JWT.decode(token, SECRET, true, algorithm: "HS256")
