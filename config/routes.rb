513c4493ae5adfb9472aff1083b8d83026a997fa
Inzamam Tahir <inzamamtahir@gmail.com>
2026-09-15 11:37:11 +0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -35,11 +35,11 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root "dashboard#index"
-  
+
     get "login", to: "sessions#new"
     post "login", to: "sessions#create"
     delete "logout", to: "sessions#destroy"
-  
+
     resources :customers, only: [:index, :show]
     resources :vehicle_makes
   end
